59c30eb0922239a43f74553ddc1f5814d0efd273
Sooki <0xSooki@gmail.com>
2023-02-10 11:13:55 +0100

    Move icons to assets

--- packages/nextjs-app-ts/src/components/shared/Navbar/ConnectButton.tsx
+++ packages/nextjs-app-ts/src/components/shared/Navbar/ConnectButton.tsx
@@ -1,7 +1,10 @@
 import { ConnectButton as RConnectButton } from '@rainbow-me/rainbowkit';
+import Image from 'next/image';
 import { ReactElement } from 'react';
 
 import Button from '../Button';
+
+import Wallet from '/public/assets/wallet.svg';
 
 import WalletMenu from './WalletMenu';
 
@@ -41,28 +44,12 @@ const ConnectButton = (): JSX.Element => {
               }
 
               return (
-                <div className="flex gap-3">
-                  <div className="dropdown dropdown-end font-signika">
-                    <div tabIndex={0} className="btn btn-secondary btn-sm text-base">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="icon icon-tabler icon-tabler-wallet mr-2"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        strokeWidth="2"
-                        stroke="currentColor"
-                        fill="none"
-                        strokeLinecap="round"
-                        strokeLinejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                        <path d="M17 8v-3a1 1 0 0 0 -1 -1h-10a2 2 0 0 0 0 4h12a1 1 0 0 1 1 1v3m0 4v3a1 1 0 0 1 -1 1h-12a2 2 0 0 1 -2 -2v-12"></path>
-                        <path d="M20 12v4h-4a2 2 0 0 1 0 -4h4"></path>
-                      </svg>
-                      <label className="cursor-pointer normal-case">{account.displayName}</label>
-                    </div>
-                    <WalletMenu />
+                <div className="dropdown dropdown-end font-signika">
+                  <div tabIndex={0} className="btn flex gap-2 btn-secondary btn-sm text-base">
+                    <Image src={Wallet} height={24} width={24} alt="Wallet" />
+                    <label className="cursor-pointer normal-case">{account.displayName}</label>
                   </div>
+                  <WalletMenu />
                 </div>
               );
             })()}
