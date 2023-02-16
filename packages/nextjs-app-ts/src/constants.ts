3602481b4f86faa9d907083c627c3121b3d67e29
Sooki <0xSooki@gmail.com>
2023-02-16 11:49:33 +0100

    Create main page

--- packages/nextjs-app-ts/src/constants.ts
+++ packages/nextjs-app-ts/src/constants.ts
@@ -1,7 +1,54 @@
+import CoverImage from '/public/assets/cover_image.svg';
+
 const MENU_LIST = [
   { name: 'Campaigns', href: '/campaigns' },
   { name: 'About', href: '/about' },
   { name: 'Team', href: '/team' },
 ];
 
-export { MENU_LIST };
+const CAMPAIGNS = [
+  {
+    title: 'Potatoes for everybody',
+    description: 'This is brief description of the running campaign. Don’t know what to tell more ab...',
+    image: CoverImage,
+    time: '3 days',
+    participants: 23,
+  },
+  {
+    title: 'Potatoes for everybody',
+    description: 'This is brief description of the running campaign. Don’t know what to tell more ab...',
+    image: CoverImage,
+    time: '3 days',
+    participants: 23,
+  },
+  {
+    title: 'Potatoes for everybody',
+    description: 'This is brief description of the running campaign. Don’t know what to tell more ab...',
+    image: CoverImage,
+    time: '3 days',
+    participants: 23,
+  },
+  {
+    title: 'Potatoes for everybody',
+    description: 'This is brief description of the running campaign. Don’t know what to tell more ab...',
+    image: CoverImage,
+    time: '3 days',
+    participants: 23,
+  },
+  {
+    title: 'Potatoes for everybody',
+    description: 'This is brief description of the running campaign. Don’t know what to tell more ab...',
+    image: CoverImage,
+    time: '3 days',
+    participants: 23,
+  },
+  {
+    title: 'Potatoes for everybody',
+    description: 'This is brief description of the running campaign. Don’t know what to tell more ab...',
+    image: CoverImage,
+    time: '3 days',
+    participants: 23,
+  },
+];
+
+export { MENU_LIST, CAMPAIGNS };
--- packages/nextjs-app-ts/src/pages/index.tsx
+++ packages/nextjs-app-ts/src/pages/index.tsx
@@ -1,16 +1,49 @@
 import Head from 'next/head';
+import Image from 'next/image';
+
+import Button from '~~/components/shared/Button';
+import CampaignCard from '~~/components/shared/CampaignCard';
+import Footer from '~~/components/shared/Footer';
+import Navbar from '~~/components/shared/Navbar/Navbar';
+import { CAMPAIGNS } from '~~/constants';
+
+import Title from '/public/assets/title.svg';
 
 const Page = (): JSX.Element => {
   return (
-    <div className="App">
-      <Head>
-        <title>Potato Potata</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div>
-        <h1 className="text-4xl text-primary-500 font-bold">Welcome to Potato Potata</h1>
+    <>
+      <div className="App w-full">
+        <Head>
+          <title>Potato Potata</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <div>
+          <Navbar />
+          <div className="flex justify-center mx-48">
+            <Image src={Title} />
+          </div>
+          <div className="mt-16 flex w-full flex-col gap-12 items-center justify-center">
+            <h1 className="font-signika font-bold text-4xl text-primary">Active Campaigns</h1>
+            <div className="grid lg:grid-cols-3 md:grid-cols-2 items-center gap-y-12 mx-8 gap-x-8">
+              {CAMPAIGNS.map((campaign, index) => (
+                <CampaignCard key={index} campaign={campaign} />
+              ))}
+            </div>
+          </div>
+          <div className="flex justify-center mb-16 mt-12">
+            <Button
+              size="md"
+              onClick={(): void => {
+                console.log('loading...');
+              }}>
+              LOAD MORE CAMPAIGNS
+            </Button>
+          </div>
+
+          <Footer />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
