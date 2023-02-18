ae42f4a086dc638eb9e7f798034831a6070ff46d
Sooki <0xSooki@gmail.com>
2023-02-18 21:43:05 +0100

    Add type to input component

--- packages/nextjs-app-ts/src/components/shared/Input.tsx
+++ packages/nextjs-app-ts/src/components/shared/Input.tsx
@@ -4,6 +4,7 @@ import { CompConfig } from '~~/helpers/types/components';
 
 interface Props {
   placeholder: string;
+  type: 'text' | 'number';
   size?: 'xs' | 'sm' | 'md' | 'lg';
   onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
   className?: string;
@@ -24,14 +25,14 @@ const labelConfig: CompConfig = {
   lg: 'text-base',
 };
 
-const Input: FC<Props> = ({ size = 'sm', placeholder, label, className = '', onChange }): JSX.Element => {
+const Input: FC<Props> = ({ size = 'sm', type, placeholder, label, className = '', onChange }): JSX.Element => {
   return (
     <div className="form-control w-full max-w-xs">
       <label className="label pb-1">
         <span className={`label-text text-primary font-medium ${labelConfig[size]}`}>{label}</span>
       </label>
       <input
-        type="text"
+        type={type}
         placeholder={placeholder}
         onChange={onChange}
         className={`input focus:outline-none border-primary border-opacity-20 border-1.5 font-signika placeholder:opacity-50 w-full max-w-xs ${inputConfig[size]} ${className}`}
