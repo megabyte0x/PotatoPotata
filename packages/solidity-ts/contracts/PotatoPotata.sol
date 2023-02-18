a676a08ad257a1aeb44270200418b2035b365298
Sooki <0xSooki@gmail.com>
2023-02-18 14:45:32 +0100

    Move campaign details to contract

--- packages/solidity-ts/contracts/PotatoPotata.sol
+++ packages/solidity-ts/contracts/PotatoPotata.sol
@@ -6,22 +6,22 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./Campaign.sol";
 
 contract PotatoPotata is Ownable {
-  mapping(address => CampaignDetails) campaigns;
-
-  struct CampaignDetails {
-    address campaignAddress;
-    string ipfsUrl;
-  }
-
-  mapping(address => string) campaignUrls;
+  mapping(address => address[]) campaigns;
 
   Campaign campaignContract;
 
-  function registerCamapaign(string calldata ipfsUrl) external returns (address) {
-    campaignContract = new Campaign();
-    campaigns[msg.sender].campaignAddress = address(campaignContract);
-    campaigns[msg.sender].ipfsUrl = ipfsUrl;
+  function registerCamapaign(
+    string memory _name,
+    string memory _descriptionCID,
+    string memory _imageCID
+  ) external returns (address) {
+    campaignContract = new Campaign(_name, _descriptionCID, _imageCID);
+    campaigns[msg.sender].push(address(campaignContract));
 
     return address(campaignContract);
+  }
+
+  function getCampaignAddress(address _address) external view returns (address[] memory) {
+    return campaigns[_address];
   }
 }
